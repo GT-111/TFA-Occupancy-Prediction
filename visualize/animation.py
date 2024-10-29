076a2d5289f6894ed0c2e9d03ca79a377d61be39
Hetian <hg25079@uga.edu>
2024-10-28 21:18:27 -0400

    Refactor Code

--- visualize/animation.py
+++ visualize/animation.py
@@ -1,143 +1,227 @@
 from utils.file_utils import get_config
-import numpy as np
-import plotly.figure_factory as ff
-import plotly.graph_objects as go
 from dataset.occ_flow_utils import GridMap
+from dataset.I24Dataset import I24Dataset
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib.colors import LinearSegmentedColormap
+import flow_vis
+import numpy as np
 
-
-# Load data
-config = get_config()
-data = np.load('/hdd/HetianGuo/I24/63858a2cfb3ff533c12df166_112.0_36.0.npy', allow_pickle=True).item()
-grid_map = GridMap(config)
-occupancy_map, flow = grid_map.get_map_flow(data)
-
-# Grid size
-grid_size_x = config.dataset.grid_size_x
-grid_size_y = config.dataset.grid_size_y
-x, y = np.meshgrid(np.arange(grid_size_x), np.arange(grid_size_y))
-
-# Layout for the Plotly figure
-layout = go.Layout(
-    width=800, height=800,
-    xaxis=dict(range=[0, grid_size_x], title='X'),
-    yaxis=dict(range=[0, grid_size_y], title='Y', scaleanchor='x'),
-    showlegend=False,
-    title='Occupancy Map and Flow Visualization',
-    plot_bgcolor='rgba(0,0,0,0)',
-    updatemenus=[  # Play/Pause controls
-        dict(
-            type='buttons',
-            showactive=False,
-            buttons=[
-                dict(
-                    label='Play',
-                    method='animate',
-                    args=[None, {
-                        'frame': {'duration': 500, 'redraw': False},
-                        
-                        'transition': {'duration': 300}
-                    }]
-                ),
-                dict(
-                    label='Pause',
-                    method='animate',
-                    args=[[None], {
-                        'frame': {'duration': 0, 'redraw': False},
-                        'mode': 'immediate',
-                        'transition': {'duration': 0}
-                    }]
-                )
-            ]
-        )
-    ],
-    sliders=[  # Slider for controlling frames
-        dict(
-            steps=[
-                dict(
-                    method='animate',
-                    args=[[str(idx)], {
-                        'mode': 'immediate',
-                        'frame': {'duration': 300},
-                        'transition': {'duration': 300}
-                    }],
-                    label=str(idx)
-                ) for idx in range(flow.shape[0])
-            ],
-            active=0, transition={'duration': 300},
-            x=0.1, y=0, len=0.9
-        )
-    ]
-)
-
-# Initialize frames
-frames = []
-
-# Generate frames with quiver arrows, heatmap, and road lines
-for idx in range(flow.shape[0]):
-    traces = []
-
-    # Add road lines
-    road_lines_y = [12, 24, 36, 48, 60, -12, -24, -36, -48, -60]
-    for road_line_y in road_lines_y:
-        y_coord = road_line_y * grid_size_y / 160 + grid_size_y / 2
-        traces.append(go.Scatter(
-            x=[0, grid_size_x],  # Start and end on x-axis
-            y=[y_coord, y_coord],  # Horizontal line at y
-            mode='lines',
-            line=dict(color='black', width=1)
-        ))
-
-    # Extract map and flow data for the current frame
-    flow_frame = flow[idx].swapaxes(0, 1)
-    map_frame = occupancy_map[idx].swapaxes(0, 1)
-    valid_mask = (flow_frame[:, :, 0] != 0) | (flow_frame[:, :, 1] != 0)
-    # Add quiver plot (if valid flow data exists)
-    if flow_frame[valid_mask].shape[0] > 0:
-        
-        quiver_fig = ff.create_quiver(
-            x[valid_mask], y[valid_mask],
-            flow_frame[valid_mask][:, 0], flow_frame[valid_mask][:, 1],
-            scale=0.2,
-            line=dict(color='black', width=2)
-        )
-        
-        traces.extend(quiver_fig.data)  # Add quiver traces
-    else:
-        # add blank quiver plot
-        quiver_fig = ff.create_quiver(
-            x=[0], y=[0],
-            u=[0], v=[0],
-            scale=0.2,
-            line=dict(color='black', width=2)
-        )
-        traces.extend(quiver_fig.data)
-    # Add heatmap (background)
-    traces.append(go.Heatmap(
-        z=map_frame,
-        x=np.arange(grid_size_x),
-        y=np.arange(grid_size_y),
-        colorscale=[
-            [0.0, 'rgba(0,0,0,0)'],
-            [0.5, 'rgba(0,0,0,1)'],
-            [1.0, 'rgba(0,0,0,1)']],
-        opacity=0.2,
-        zmin=0,
-        zmax=1,
-        showscale=False
-    ))
-
+def process_data_for_visualization(data_dic):
+    prv_data_dic = data_dic['prv']
+    cur_data_dic = data_dic['cur']
+    nxt_data_dic = data_dic['nxt']
+    prv_scene_occ_ogm, prv_scene_obs_ogm, prv_scene_flow_map = gridmap.get_map_flow(prv_data_dic)
+    cur_scene_occ_ogm, cur_scene_obs_ogm, cur_scene_flow_map = gridmap.get_map_flow(cur_data_dic)
+    nxt_scene_occ_ogm, nxt_scene_obs_ogm, nxt_scene_flow_map = gridmap.get_map_flow(nxt_data_dic)
+    prv_scene_occ_ogm, prv_scene_obs_ogm, prv_scene_flow_map = prv_scene_occ_ogm.swapaxes(-2, -1), prv_scene_obs_ogm.swapaxes(-2, -1), prv_scene_flow_map.swapaxes(-2, -3)
+    cur_scene_occ_ogm, cur_scene_obs_ogm, cur_scene_flow_map = cur_scene_occ_ogm.swapaxes(-2, -1), cur_scene_obs_ogm.swapaxes(-2, -1), cur_scene_flow_map.swapaxes(-2, -3)
+    nxt_scene_occ_ogm, nxt_scene_obs_ogm, nxt_scene_flow_map = nxt_scene_occ_ogm.swapaxes(-2, -1), nxt_scene_obs_ogm.swapaxes(-2, -1), nxt_scene_flow_map.swapaxes(-2, -3)
+    # change dtype to float32
+    prv_scene_occ_ogm = prv_scene_occ_ogm.astype(np.float32)
+    cur_scene_occ_ogm = cur_scene_occ_ogm.astype(np.float32)
+    nxt_scene_occ_ogm = nxt_scene_occ_ogm.astype(np.float32)
     
-    # fig = go.Figure(data=traces)
-    # fig.show()
-    # Create a frame with all traces
-    frame = go.Frame(data=traces, name=str(idx))
-    frames.append(frame)
+    prv_scene_obs_ogm = prv_scene_obs_ogm.astype(np.float32)
+    cur_scene_obs_ogm = cur_scene_obs_ogm.astype(np.float32)
+    nxt_scene_obs_ogm = nxt_scene_obs_ogm.astype(np.float32)
 
-# Initialize the figure with the first frame's data
-fig = go.Figure(data=frames[0].data, layout=layout, frames=frames)
 
-# Display the figure
-fig.show()
 
-import gif
-gif.save(frames, 'example.gif', duration=100)
+    x = np.arange(0, cur_scene_flow_map.shape[2])  # X-axis points (256)
+    y = np.arange(0, cur_scene_flow_map.shape[1])  # Y-axis points (128)
+    X, Y = np.meshgrid(x, y)
+
+
+
+    prv_scene_flow_map_rendered = np.sum(prv_scene_flow_map, axis=0)
+    cur_scene_flow_map_rendered = np.sum(cur_scene_flow_map, axis=0)
+    nxt_scene_flow_map_rendered = np.sum(nxt_scene_flow_map, axis=0)
+    
+    return X, Y, prv_scene_occ_ogm, cur_scene_occ_ogm, nxt_scene_occ_ogm, prv_scene_obs_ogm, cur_scene_obs_ogm, nxt_scene_obs_ogm, prv_scene_flow_map, cur_scene_flow_map, nxt_scene_flow_map, prv_scene_flow_map_rendered, cur_scene_flow_map_rendered, nxt_scene_flow_map_rendered
+
+
+def set_white_to_transparent(image):
+    """
+    Converts white pixels in a normalized single-channel grayscale image (values between 0 and 1) 
+    to transparent in RGBA format.
+
+    Parameters:
+    - image: NumPy array of shape (H, W) with pixel values in the range [0, 1].
+
+    Returns:
+    - rgba_image: NumPy array of shape (H, W, 4) with white pixels transparent.
+    """
+    # Ensure the input is a 2D grayscale image with normalized values
+    if len(image.shape) != 2 or not np.all((image >= 0) & (image <= 1)):
+        raise ValueError("Input must be a 2D single-channel grayscale image with values in the range [0, 1].")
+
+    # Convert grayscale to RGB by stacking the grayscale values along 3 channels
+    rgb_image = np.stack([image] * 3, axis=-1)
+
+    # Convert to RGBA by adding an alpha channel initialized to 1 (fully opaque)
+    rgba_image = np.dstack([rgb_image, np.ones_like(image)])
+
+    # Identify white pixels (value == 1) and set their alpha to 0 (fully transparent)
+    white_pixels = (image == 1)
+    rgba_image[white_pixels, 3] = 0
+
+    return rgba_image
+
+def downsample(x, y, flow_map, down_sample_x=8, down_sample_y=3):
+    """Downsample x, y, and flow_map with different rates for x and y axes."""
+    x_down = x[::down_sample_y, ::down_sample_x]
+    y_down = y[::down_sample_y, ::down_sample_x]
+    flow_map_down = flow_map[:, ::down_sample_y, ::down_sample_x, :]
+    return x_down, y_down, flow_map_down
+
+def initialize_quiver(ax, flow_map, x, y):
+    """Initialize the quiver plot with downsampled data."""
+    x, y, flow_map = downsample(x, y, flow_map)
+    mask = (flow_map[0, ..., 0] != 0) | (flow_map[0, ..., 1] != 0)
+    X_masked, Y_masked = x[mask], y[mask]
+    U_masked, V_masked = flow_map[0, ..., 0][mask], flow_map[0, ..., 1][mask]
+    return ax.quiver(X_masked, Y_masked, U_masked, V_masked, 
+                     angles='xy', scale_units='xy', scale=0.25, width=0.005, color='black', alpha=0.5)
+
+
+def get_cmap(colors):
+    
+    return LinearSegmentedColormap.from_list('while-colors', colors, N=256)
+
+def visualize(data_dic, name, vis_occ=True, vis_flow=True, vis_optical_flow=True):
+    X, Y, prv_scene_occ_ogm, cur_scene_occ_ogm, nxt_scene_occ_ogm, prv_scene_obs_ogm, cur_scene_obs_ogm, nxt_scene_obs_ogm, prv_scene_flow_map, cur_scene_flow_map, nxt_scene_flow_map, prv_scene_flow_map_rendered, cur_scene_flow_map_rendered, nxt_scene_flow_map_rendered = process_data_for_visualization(data_dic)
+    num_time_steps = cur_scene_occ_ogm.shape[0]
+    # Define the custom colormap: white from 0 to 0.5, then gradually red from 0.5 to 1
+    colors_occ = [
+        (1, 1, 1),  # White for 0
+        (1, 0.8, 0.8),  # Light red for mid-range
+        (0, 1, 0)  # Red for 1
+    ]
+    cmap_occ = get_cmap(colors_occ)
+    
+    colors_obs = [
+        (1, 1, 1),  # White for 0
+        (1, 0.8, 0.8),  # Light red for mid-range
+        (1, 0, 1)  # Red for 1
+    ]
+    cmap_obs = get_cmap(colors_obs)
+    
+
+    # Initialize the plot
+    fig, axes = plt.subplots(1, 3, figsize=(15, 5), gridspec_kw={'wspace': 0, 'hspace': 0})
+    fig.subplots_adjust(left=0, right=1, top=1, bottom=0, wspace=0, hspace=0)
+    # # Create the meshgrid for the quiver plot
+    # blue_square = plt.Line2D([0], [0], marker='s', color='w', markerfacecolor='blue', markersize=15, label='Blue Square')
+    # red_square = plt.Line2D([0], [0], marker='s', color='w', markerfacecolor='red', markersize=15, label='Red Square')
+    # plt.legend(handles=[blue_square, red_square], loc='best')
+    for ax in axes:
+        ax.axis('off')
+
+    axes[0].set_title('Previous Scene')
+    axes[1].set_title('Current Scene')
+    # axes[2].set_title('Next Scene')
+    if vis_optical_flow:
+        
+        flow_rendered_prv = axes[0].imshow(flow_vis.flow_to_color(prv_scene_flow_map_rendered),interpolation='nearest', alpha=1)
+        flow_rendered_cur = axes[1].imshow(flow_vis.flow_to_color(cur_scene_flow_map_rendered),interpolation='nearest', alpha=1)
+        flow_rendered_nex = axes[2].imshow(flow_vis.flow_to_color(nxt_scene_flow_map_rendered),interpolation='nearest', alpha=1)
+    # Initialize the occupancy map with the first frame's data
+    if vis_occ:
+        
+        img_prv_occ_ogm = axes[0].imshow(prv_scene_occ_ogm[0], cmap=cmap_occ, interpolation='nearest', alpha=0.5)
+        img_cur_occ_ogm = axes[1].imshow(cur_scene_occ_ogm[0], cmap=cmap_occ, interpolation='nearest', alpha=0.5)
+        img_nxt_occ_ogm = axes[2].imshow(nxt_scene_occ_ogm[0], cmap=cmap_occ, interpolation='nearest', alpha=0.5)
+        img_prv_obs_ogm = axes[0].imshow(prv_scene_obs_ogm[0], cmap=cmap_obs, interpolation='nearest', alpha=0.5)
+        img_cur_obs_ogm = axes[1].imshow(cur_scene_obs_ogm[0], cmap=cmap_obs, interpolation='nearest', alpha=0.5)
+        img_nxt_obs_ogm = axes[2].imshow(nxt_scene_obs_ogm[0], cmap=cmap_obs, interpolation='nearest', alpha=0.5)
+    if vis_flow:
+        quiver_prv = initialize_quiver(axes[0], prv_scene_flow_map, X, Y)
+        quiver_cur = initialize_quiver(axes[1], cur_scene_flow_map, X, Y)
+        quiver_nxt = initialize_quiver(axes[2], nxt_scene_flow_map, X, Y)
+
+    road_lines_y = [12, 24, 36, 48, 60, -12, -24, -36, -48, -60]
+    # Animation update function
+    def update(frame):
+        """Update both the quiver plot and the occupancy map for each frame."""
+        # Update occupancy map
+        for ax in axes:
+            ax.clear()
+            ax.axis('off')
+        axes[0].set_title('Previous Scene')
+        axes[1].set_title('Current Scene')
+        axes[2].set_title('Next Scene')
+        axes[0].scatter(X[::8, ::8], Y[::8, ::8], color='black', s=1, alpha=0.5)
+        axes[1].scatter(X[::8, ::8], Y[::8, ::8], color='black', s=1, alpha=0.5)
+        axes[2].scatter(X[::8, ::8], Y[::8, ::8], color='black', s=1, alpha=0.5)   
+
+        if vis_optical_flow:
+            flow_rendered_prv = axes[0].imshow(flow_vis.flow_to_color(prv_scene_flow_map_rendered),interpolation='nearest', alpha=1)
+            flow_rendered_cur = axes[1].imshow(flow_vis.flow_to_color(cur_scene_flow_map_rendered),interpolation='nearest', alpha=1)
+            flow_rendered_nex = axes[2].imshow(flow_vis.flow_to_color(nxt_scene_flow_map_rendered),interpolation='nearest', alpha=1)
+            
+        # Initialize the occupancy map with the first frame's data
+        if vis_occ:
+            
+            img_prv_occ_ogm = axes[0].imshow(prv_scene_occ_ogm[frame], cmap=cmap_occ, interpolation='nearest', alpha=0.5)
+            img_cur_occ_ogm = axes[1].imshow(cur_scene_occ_ogm[frame], cmap=cmap_occ, interpolation='nearest', alpha=0.5)
+            img_nxt_occ_ogm = axes[2].imshow(nxt_scene_occ_ogm[frame], cmap=cmap_occ, interpolation='nearest', alpha=0.5)
+            img_prv_obs_ogm = axes[0].imshow(prv_scene_obs_ogm[frame], cmap=cmap_obs, interpolation='nearest', alpha=0.5)
+            img_cur_obs_ogm = axes[1].imshow(cur_scene_obs_ogm[frame], cmap=cmap_obs, interpolation='nearest', alpha=0.5)
+            img_nxt_obs_ogm = axes[2].imshow(nxt_scene_obs_ogm[frame], cmap=cmap_obs, interpolation='nearest', alpha=0.5)
+            
+        def create_quiver(ax, flow_map, x, y):
+            x, y, flow_map = downsample(x, y, flow_map)
+            mask = (flow_map[frame, ..., 0] != 0) | (flow_map[frame, ..., 1] != 0)
+            X_masked, Y_masked = x[mask], y[mask]
+            U_masked, V_masked = flow_map[frame, ..., 0][mask], flow_map[frame,..., 1][mask]
+            return ax.quiver(X_masked, Y_masked, U_masked, V_masked,
+                             angles='xy', scale_units='xy', scale=0.25, width = 0.005, color='black', alpha=0.5)
+
+        quiver_prv = create_quiver(axes[0], prv_scene_flow_map, X, Y)
+        quiver_cur = create_quiver(axes[1], cur_scene_flow_map, X, Y)
+        quiver_nxt = create_quiver(axes[2], nxt_scene_flow_map, X, Y)
+        for ax in axes:
+            for road_line_y in road_lines_y:
+                y_coord = road_line_y * grid_size_y / 160 + grid_size_y / 2
+                ax.hlines(
+                    y=y_coord, xmin=0, xmax=grid_size_x,  # Start and end points on the x-axis
+                    colors='black', linewidth=1
+                )
+        #
+        return [quiver_prv, quiver_cur, quiver_nxt]
+
+    # Create the animation
+
+    ani = animation.FuncAnimation(fig, update, frames=num_time_steps, interval=50, blit=True)
+
+    # Save the animation without padding or extra margins
+    ani.save(name + '.mp4', writer='ffmpeg', fps=20, dpi=300)
+
+
+if __name__ == '__main__':
+    config = get_config()
+    gridmap = GridMap(config)
+    dataset = I24Dataset(config)
+    grid_size_x = config.dataset.grid_size_x
+    grid_size_y = config.dataset.grid_size_y
+    name = "scene_19915"
+    test_data = np.load(config.dataset.processed_data + '/' + name + '.npy', allow_pickle=True).item()
+
+    config = get_config()
+    files = []
+    for i in range(20000):
+        file_name = config.dataset.processed_data + '/scene_{}.npy'.format(i)
+        test_data = np.load(file_name, allow_pickle=True).item()
+        prv_data_dic = test_data['prv']
+        cur_data_dic = test_data['cur']
+        nxt_data_dic = test_data['nxt']
+        # print(test_data['cur']['direction'])
+        # if cur_data_dic['direction'] has 1 and -1
+        if 1 in cur_data_dic['direction'] and -1 in cur_data_dic['direction']:
+            num = np.sum(cur_data_dic['direction'] == 1)
+            num_r = np.sum(cur_data_dic['direction'] == -1)
+            if num > 5 and num_r > 5:
+                files.append(file_name)
+                print(f'{file_name} has {num} cars going forward and {num_r} cars going backward')
+    visualize(test_data, name, vis_occ=True, vis_flow=True, vis_optical_flow=True)
