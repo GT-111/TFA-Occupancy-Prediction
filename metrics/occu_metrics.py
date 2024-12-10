Bug Fix!!!!!!! The incorrect flow calculation issue has been resolved

metrics/occu_metrics.py
@@ -232,8 +232,8 @@ def _flow_warp(
   # [height, width, 2] but storing x, y coordinates.
   identity_indices = torch.stack(
       (
-          w_idx,
-          h_idx,
+          w_idx.T,
+          h_idx.T,
       ),
       dim=-1,
   )
@@ -258,6 +258,7 @@ def _flow_warp(
     # warped_indices = warped_indices + 1
     # NOTE: tensorflow graphics expects warp to contain (x, y) as well.
     # [batch_size, height, width, 2]
+    print(flow_origin_occupancy.shape)
     warped_origin = sample(
         image=flow_origin_occupancy,
         warp=warped_indices,

preprocess/csv2npys.py
@@ -25,17 +25,16 @@ temporal_window = config.preprocessing.temporal_window
 spatial_length = int(np.floor((x_max - x_min - spatial_window) / spatial_stride))
 temporal_length = int(np.floor((time_max - time_min - temporal_window) / temporal_stride))
 
-his_len = config.task_config.history_length
-pred_len = config.task_config.prediction_length
+history_length = config.task_config.history_length
+prediction_length = config.task_config.prediction_length
+num_waypoints = config.task_config.num_waypoints
+num_his_points = config.task_config.num_his_points
+
 def get_scene_data(idx_list):
     prv_idx, cur_idx, nxt_idx = idx_list
-    
-    
     prv_spatial_start, _, prv_temporal_start, prv_temporal_end = idx2range(prv_idx)
     _, nxt_spatial_end, _, _ = idx2range(nxt_idx)
     
-    # scene_data = data_df[(data_df['timestamp'].between(prv_temporal_start, prv_temporal_end))]
-    # scene_data = scene_data[(scene_data['x_position'].between(prv_spatial_start, nxt_spatial_end))]
     # time stamp is the index, use the index to slice the data
     scene_data = data_df.loc[prv_temporal_start:prv_temporal_end].copy()
     scene_data.loc[:, 'timestamp']= scene_data.index
@@ -63,12 +62,22 @@ def get_feature_dics(idx_list):
     nxt_feature_dic = get_feature_dic(scene_data, nxt_idx)
     return prv_feature_dic, cur_feature_dic, nxt_feature_dic
 
+@DeprecationWarning
 def add_occ_flow(feature_dic):
     occluded_occupancy_map, observed_occupancy_map, flow_map = grid_map.get_map_flow(feature_dic)
     feature_dic['occluded_occupancy_map'] = occluded_occupancy_map.astype(np.float32)
     feature_dic['observed_occupancy_map'] = observed_occupancy_map.astype(np.float32)
     feature_dic['flow_map'] = flow_map.astype(np.float32)
     return feature_dic 
+
+def convert(coordinates):
+    # coordinates (Na, timestamps)
+    coordinates = np.concatenate([coordinates[:,:history_length][:, ::(history_length//num_his_points)], 
+              coordinates[:,history_length: history_length + prediction_length][:,::(prediction_length // num_waypoints)]]
+             ,axis=1
+             )
+    
+    return coordinates
 
 def get_feature_dic(scene_data, scene_idx):
     spatial_start, spatial_end, temporal_start, temporal_end = idx2range(scene_idx)
@@ -148,21 +157,40 @@ def get_feature_dic(scene_data, scene_idx):
         # 'scene_id': idx,
         # '_id': vehicles_id,
         'num_vehicles': vehicles_num,
-        'timestamp': vehicles_timestamp,
+        'timestamp': convert(vehicles_timestamp),
         'length': vehicles_length,
         'width': vehicles_width,
         # 'height': vehicles_height,
         'direction': vehicles_direction,
         'class':vehicles_class,
-        'x_position': vehicles_x,
-        'y_position': vehicles_y,
-        'x_velocity': vehicles_x_velocity,
-        'y_velocity': vehicles_y_velocity,
-        'yaw_angle': vehicles_yaw_angle,
-        'start_pos': spatial_start,
-        'start_time': temporal_start,
+        'x_position': convert(vehicles_x),
+        'y_position': convert(vehicles_y),
+        'x_velocity': convert(vehicles_x_velocity),
+        'y_velocity': convert(vehicles_y_velocity),
+        'yaw_angle': convert(vehicles_yaw_angle),
+        # 'start_pos': spatial_start,
+        # 'start_time': temporal_start,
     }
+    
     return result_dict
+
+def get_trajectories(data, config):
+    
+    num_time_steps = config.task_config.num_his_points + config.task_config.num_waypoints
+    observed_idx = (data['timestamp'][...,config.task_config.num_his_points - 1] > 0)
+    
+    # 'cur/meta/length', 'cur/meta/width', 'cur/meta/class', 'cur/meta/direction'
+    vector_features_list = ['length', 'width', 'class', 'direction']
+    node_features_list = ['timestamp', 'x_position', 'y_position', 'x_velocity', 'y_velocity', 'yaw_angle']
+
+    vector_features_observed = np.stack([data[feature][observed_idx][:, None].repeat(num_time_steps, axis = 1) for feature in vector_features_list], axis = -1)
+    vector_features_occluded = np.stack([data[feature][~observed_idx][:, None].repeat(num_time_steps, axis = 1) for feature in vector_features_list], axis = -1)
+    node_features_observed = np.stack([data[feature][observed_idx] for feature in node_features_list], axis = -1)
+    node_features_occluded = np.stack([data[feature][~observed_idx] for feature in node_features_list], axis = -1)
+    observed_trajectories = np.concatenate([node_features_observed, vector_features_observed], axis = -1)
+    occluded_trajectories = np.concatenate([node_features_occluded, vector_features_occluded], axis = -1)
+    return observed_trajectories, occluded_trajectories
+
 import typing
 import concurrent.futures
 def process_idx(idx):
@@ -181,37 +209,36 @@ def process_idx(idx):
     prv_feature_dic, cur_feature_dic, nxt_feature_dic = get_feature_dics(idx_list)
     if cur_feature_dic['num_vehicles'] <= 2:
         return
-    points_per_vehicle_history = (np.sum(cur_feature_dic['timestamp'][:, :config.task_config.history_length]!=0) / cur_feature_dic['num_vehicles'])/config.task_config.history_length
-    points_per_vehicle_future = (np.sum(cur_feature_dic['timestamp'][:, config.task_config.history_length: config.task_config.history_length + config.task_config.prediction_length]!=0) / cur_feature_dic['num_vehicles'])/config.task_config.prediction_length
-    # print(points_per_vehicle_history, points_per_vehicle_future)
-    if points_per_vehicle_history < 0.4 or points_per_vehicle_future < 0.4: # type: ignore
+    points_per_vehicle_history = (np.sum(cur_feature_dic['timestamp'][:, :num_his_points]!=0) / cur_feature_dic['num_vehicles'])/num_his_points
+    points_per_vehicle_future = (np.sum(cur_feature_dic['timestamp'][:, num_his_points: num_his_points + num_waypoints]!=0) / cur_feature_dic['num_vehicles'])/num_waypoints
+    # if the number of valid points per vehicle is less than 0.4, skip the scene
+    if points_per_vehicle_history < 0.4 or points_per_vehicle_future < 0.4:
         return
-    
+
+    occluded_occupancy_map, observed_occupancy_map, flow_map = grid_map.get_map_flow(cur_feature_dic)
+    observed_trajectories, occluded_trajectories = get_trajectories(cur_feature_dic, config)
     result_dic = {
-        # 'prv': add_occ_flow(prv_feature_dic),
-        'cur': add_occ_flow(cur_feature_dic),
-        # 'nxt': add_occ_flow(nxt_feature_dic)
+        'occluded_occupancy_map': occluded_occupancy_map, # H,W,T
+        'observed_occupancy_map': observed_occupancy_map, # H,W,T
+        'flow_map': flow_map, # H,W,(T-1),2
+        'observed_trajectories': observed_trajectories, # (Nobs, T, D)
+        'occluded_trajectories': occluded_trajectories, # (Nocc, T, D)
     }
     
-    num_waypoints = config.task_config.num_waypoints
-    num_his_points = config.task_config.num_his_points
     feature_dic = typing.DefaultDict(dict)
-    for dic_k, dic in result_dic.items():
-        for k, v in dic.items():
-            if k in ['timestamp', 'x_position', 'y_position', 'x_velocity', 'y_velocity', 'yaw_angle']:
-                # (Num of Agents, Timestamp
-                feature_dic[dic_k + '/state/his/' + k] = v[:, :config.task_config.history_length]
-                feature_dic[dic_k + '/state/pred/' + k] = v[:, config.task_config.history_length: config.task_config.history_length + config.task_config.prediction_length]
-            elif k in ['occluded_occupancy_map', 'observed_occupancy_map', 'flow_map']:
-                # (Timestamp, H, W) -> Occ (Timestamp, H, W, 2) -> Flow
-                feature_his =  v[:num_his_points,...]
-                feature_dic[dic_k + '/state/his/' + k] = feature_his
-                feature_pred = v[num_his_points: num_his_points + num_waypoints,...]
-                feature_dic[dic_k + '/state/pred/' + k] = feature_pred
-                
-            else:
-                feature_dic[dic_k + '/meta/' + k] = v
-                
+    feature_dic['his/occluded_occupancy_map'] = result_dic['occluded_occupancy_map'][..., :num_his_points]
+    feature_dic['pred/occluded_occupancy_map'] = result_dic['occluded_occupancy_map'][..., num_his_points: num_his_points + num_waypoints]
+    feature_dic['his/observed_occupancy_map'] = result_dic['observed_occupancy_map'][..., :num_his_points]
+    feature_dic['pred/observed_occupancy_map'] = result_dic['observed_occupancy_map'][..., num_his_points: num_his_points + num_waypoints]
+    feature_dic['his/flow_map'] = result_dic['flow_map'][..., :-num_waypoints, :]
+    feature_dic['pred/flow_map'] = result_dic['flow_map'][..., -num_waypoints:, :]
+    feature_dic['his/observed_trajectories'] = result_dic['observed_trajectories'][..., :num_his_points]
+    feature_dic['pred/observed_trajectories'] = result_dic['observed_trajectories'][..., num_his_points: num_his_points + num_waypoints]
+    feature_dic['his/occluded_trajectories'] = result_dic['occluded_trajectories'][..., :num_his_points]
+    feature_dic['pred/occluded_trajectories'] = result_dic['occluded_trajectories'][..., num_his_points: num_his_points + num_waypoints]
+    all_occupancy_map = result_dic['occluded_occupancy_map'] + result_dic['observed_occupancy_map']
+    all_occupancy_map = np.clip(all_occupancy_map, 0, 1)
+    feature_dic['flow_origin_occupancy_map'] = all_occupancy_map[...,num_his_points - 1: num_his_points -1 + num_waypoints]
     np.save(f'{config.paths.processed_data}/scene_{idx}', feature_dic)
 
 # # # Number of threads to use
@@ -223,4 +250,3 @@ total_len = 1500000
 with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
     # Wrap tqdm around the executor to show progress
     list(tqdm(executor.map(process_idx, range(total_len)), total=total_len))
-# process_idx(1014)

train.py
@@ -50,7 +50,7 @@ def setup(config, gpu_id):
                            no_use_warp=True,
                            use_pred=False,
                            use_focal_loss=True,
-                           use_gt=True)
+                           use_gt=False)
     
     optimizer = torch.optim.NAdam(params=model.parameters(), 
                                   lr=config.training_settings.optimizer.learning_rate,
@@ -71,7 +71,7 @@ def model_training(gpu_id, world_size, config):
     model, loss_fn, optimizer, scheduler = setup(config, gpu_id)
     train_dataloader, val_dataloader, _ = get_dataloader_ddp(config)
     global_step = 0
-    road_map = torch.from_numpy(get_road_map(config)).to(gpu_id, dtype=torch.float32)
+    
     if get_last_checkpoint(config.paths.checkpoints) is not None:
         
         checkpoint = torch.load(get_last_checkpoint(config.paths.checkpoints))
@@ -101,29 +101,29 @@ def model_training(gpu_id, world_size, config):
         
         train_dataloader.sampler.set_epoch(epoch)
         loop = tqdm(enumerate(train_dataloader), total=len(train_dataloader))
-        for batch, data in loop:
-            
+        for batch_idx, data in loop:
+            batch_size = data['his/observed_occupancy_map'].shape[0]
             input_dict, ground_truth_dict = training_utils.parse_data(data, gpu_id, config)
-            his_occupancy_map = input_dict['cur/state/his/observed_occupancy_map']
-            his_flow_map = input_dict['cur/state/his/flow_map']
+            
+            # get the input
+            his_occupancy_map = torch.clamp(input_dict['his/observed_occupancy_map'] + input_dict['his/occluded_occupancy_map'], 0, 1)
+            his_flow_map = input_dict['his/flow_map']
+            his_occluded_trajectories = input_dict['his/occluded_trajectories']
+            his_observed_trajectories = input_dict['his/observed_trajectories']
+            flow_origin_occupancy = input_dict['flow_origin_occupancy_map']
+            
+            # get the ground truth
+            gt_occluded_occupancy_logits = ground_truth_dict['pred/occluded_occupancy_map']
+            gt_observed_occupancy_logits = ground_truth_dict['pred/observed_occupancy_map']
+            gt_flow = ground_truth_dict['pred/flow_map']
+            
+            road_map = torch.from_numpy(get_road_map(config, batch_size)).to(gpu_id, dtype=torch.float32)
             
             
-            obs_traj, occ_traj= get_trajs(input_dict, config)
-            # self,occupancy_map, flow_map, road_map, obs_traj, occ_traj
-            his_occupancy_map = his_occupancy_map.permute([0,2,3,1]) # B H W T
-            his_flow_map = his_flow_map[:,-1,:,:,:] # B H W 2
-            outputs = model(his_occupancy_map, his_flow_map, road_map, obs_traj, occ_traj)
+            his_flow_map = his_flow_map[...,-1,:] # B H W 2
+            outputs = model(his_occupancy_map, his_flow_map, road_map, his_observed_trajectories, his_occluded_trajectories)
             pred_observed_occupancy_logits, pred_occluded_occupancy_logits, pred_flow_logits = training_utils.parse_outputs(outputs, config.task_config.num_waypoints)
-            gt_occluded_occupancy_logits = ground_truth_dict['cur/state/pred/occluded_occupancy_map']
-            gt_observed_occupancy_logits = ground_truth_dict['cur/state/pred/observed_occupancy_map']
-            B, T, H, W = gt_observed_occupancy_logits.shape
-            gt_occluded_occupancy_logits = gt_occluded_occupancy_logits.reshape(B, T, H, W, 1)
-            gt_observed_occupancy_logits = gt_observed_occupancy_logits.reshape(B, T, H, W, 1)
-            
-            gt_all_occupancy_logits = torch.clamp(gt_observed_occupancy_logits + gt_occluded_occupancy_logits, 0, 1)[:,:-1,...]
-            his_occupancy_map = his_occupancy_map.permute([0, 3, 1, 2]) # B H W T - > B T H W
-            flow_origin_occupancy = torch.cat([his_occupancy_map[:, -1, :, :, torch.newaxis][:,None,...], gt_all_occupancy_logits], dim=1)
-            gt_flow = ground_truth_dict['cur/state/pred/flow_map']
+    
             
             loss_dict = loss_fn(pred_observed_occupancy_logits, pred_occluded_occupancy_logits, pred_flow_logits, gt_observed_occupancy_logits, gt_occluded_occupancy_logits, gt_flow, flow_origin_occupancy)
             loss_value = torch.sum(sum(loss_dict.values()))
@@ -142,7 +142,7 @@ def model_training(gpu_id, world_size, config):
             flow_loss = train_loss_flow.compute()
             warp_loss = train_loss_warp.compute()
             if gpu_id == 0:
-                if batch % 20 == 0:
+                if batch_idx % 20 == 0:
                     logger.add_scalars(main_tag="train_loss",
                                     tag_scalar_dict={
                                         "observed_xe": obs_loss,
@@ -169,33 +169,29 @@ def model_training(gpu_id, world_size, config):
         
         with torch.no_grad():
             loop = tqdm(enumerate(val_dataloader), total=len(val_dataloader))
-            for batch, data in loop:
+            for batch_idx, data in loop:
+                batch_size = data['his/observed_occupancy_map'].shape[0]
                 input_dict, ground_truth_dict = training_utils.parse_data(data, gpu_id, config)
-                
-                his_occupancy_map = input_dict['cur/state/his/observed_occupancy_map']
-                his_flow_map = input_dict['cur/state/his/flow_map']
+
+                # get the input
+                his_occupancy_map = torch.clamp(input_dict['his/observed_occupancy_map'] + input_dict['his/occluded_occupancy_map'], 0, 1)
+                his_flow_map = input_dict['his/flow_map']
+                his_occluded_trajectories = input_dict['his/occluded_trajectories']
+                his_observed_trajectories = input_dict['his/observed_trajectories']
+                flow_origin_occupancy = input_dict['flow_origin_occupancy_map']
+
+                # get the ground truth
+                gt_occluded_occupancy_logits = ground_truth_dict['pred/occluded_occupancy_map']
+                gt_observed_occupancy_logits = ground_truth_dict['pred/observed_occupancy_map']
+                gt_flow = ground_truth_dict['pred/flow_map']
+
+                road_map = torch.from_numpy(get_road_map(config, batch_size)).to(gpu_id, dtype=torch.float32)
 
 
-                obs_traj, occ_traj= get_trajs(input_dict, config)
-                # self,occupancy_map, flow_map, road_map, obs_traj, occ_traj
-                his_occupancy_map = his_occupancy_map.permute([0,2,3,1]) # B H W T
-                his_flow_map = his_flow_map[:,-1,:,:,:] # B H W 2
-                outputs = model(his_occupancy_map, his_flow_map, road_map, obs_traj, occ_traj)
-
-                # compute losses
+                his_flow_map = his_flow_map[...,-1,:] # B H W 2
+                outputs = model(his_occupancy_map, his_flow_map, road_map, his_observed_trajectories, his_occluded_trajectories)
                 pred_observed_occupancy_logits, pred_occluded_occupancy_logits, pred_flow_logits = training_utils.parse_outputs(outputs, config.task_config.num_waypoints)
-                gt_occluded_occupancy_logits = ground_truth_dict['cur/state/pred/occluded_occupancy_map']
-                gt_observed_occupancy_logits = ground_truth_dict['cur/state/pred/observed_occupancy_map']
-                B, T, H, W = gt_observed_occupancy_logits.shape
-                gt_occluded_occupancy_logits = gt_occluded_occupancy_logits.reshape(B, T, H, W, 1)
-                gt_observed_occupancy_logits = gt_observed_occupancy_logits.reshape(B, T, H, W, 1)
-                gt_all_occupancy_logits = torch.clamp(gt_observed_occupancy_logits + gt_occluded_occupancy_logits, 0, 1)[:,:-1,...]
-                his_occupancy_map = his_occupancy_map.permute([0, 3, 1, 2]) # B H W T - > B T H W
-                flow_origin_occupancy = torch.cat([his_occupancy_map[:, -1, :, :, torch.newaxis][:,None,...], gt_all_occupancy_logits], dim=1)
-                
-                
-                
-                gt_flow = ground_truth_dict['cur/state/pred/flow_map']
+            
                 loss_dict = loss_fn(pred_observed_occupancy_logits, pred_occluded_occupancy_logits, pred_flow_logits, gt_observed_occupancy_logits, gt_occluded_occupancy_logits, gt_flow, flow_origin_occupancy)
                 loss_value = torch.sum(sum(loss_dict.values()))
                 
@@ -215,6 +211,7 @@ def model_training(gpu_id, world_size, config):
                 metrics_dict = compute_occupancy_flow_metrics(config, pred_observed_occupancy_logits, pred_occluded_occupancy_logits, pred_flow_logits, gt_observed_occupancy_logits, gt_occluded_occupancy_logits, gt_flow, flow_origin_occupancy, no_warp=False)
 
                 valid_metrics.update(metrics_dict)
+                
             val_res_dict = valid_metrics.compute()
             if gpu_id == 0:
                 logger.add_scalars(main_tag="val_metrics",

utils/dataset_utils.py
@@ -60,39 +60,38 @@ def merge_batch_by_padding_2nd_dim(tensor_list, return_pad_mask=False):
 def process_batch(batch_list):
     key_to_list = {}
     batch_size = len(batch_list)
-    meta_scalar = [
-                    'num_vehicles',
-                    # '_id', 
-                #    'scene_id', 
-                   'start_pos', 
-                   'start_time'
-                   ]
-    meta_array = ['length', 
-                  'width', 
-                #   'height', 
-                  'class', 
-                  'direction']
-    state = ['timestamp', 'x_position', 'y_position', 'x_velocity', 'y_velocity', 'yaw_angle', 'occluded_occupancy_map', 'observed_occupancy_map', 'flow_map']
+    state_keys = [
+            'his/occluded_occupancy_map', 
+            'pred/occluded_occupancy_map', 
+            'his/observed_occupancy_map', 
+            'pred/observed_occupancy_map', 
+            'his/flow_map', 
+            'pred/flow_map', 
+            'his/observed_trajectories', 
+            'pred/observed_trajectories', 
+            'his/occluded_trajectories', 
+            'pred/occluded_trajectories', 
+            'flow_origin_occupancy_map'
+            ]
     state_keys = []
     meta_scalar_keys = []
     meta_array_keys = []
     # for scene_key in ['prv', 'cur', 'nxt']:
-    for scene_key in ['cur']:
-        for key in meta_scalar:
-            key = f'{scene_key}/meta/{key}'
-            meta_scalar_keys.append(key)
-        for key in meta_array:
-            key = f'{scene_key}/meta/{key}'
-            meta_array_keys.append(key)
-        for key in state:
-            his_key = f'{scene_key}/state/his/{key}'
-            pred_key = f'{scene_key}/state/pred/{key}'
-            state_keys.extend([his_key, pred_key])
-            
-    for key in meta_scalar_keys:
-        key_to_list[key] = [batch_list[bs_idx][key] for bs_idx in range(batch_size)]
-    for key in meta_array_keys:
-        key_to_list[key] = [batch_list[bs_idx][key] for bs_idx in range(batch_size)]
+    # for scene_key in ['cur']:
+    #     for key in meta_scalar:
+    #         key = f'{scene_key}/meta/{key}'
+    #         meta_scalar_keys.append(key)
+    #     for key in meta_array:
+    #         key = f'{scene_key}/meta/{key}'
+    #         meta_array_keys.append(key)
+    #     for key in state:
+    #         his_key = f'{scene_key}/state/his/{key}'
+    #         pred_key = f'{scene_key}/state/pred/{key}'
+    #         state_keys.extend([his_key, pred_key])
+    # for key in meta_scalar_keys:
+    #     key_to_list[key] = [batch_list[bs_idx][key] for bs_idx in range(batch_size)]
+    # for key in meta_array_keys:
+    #     key_to_list[key] = [batch_list[bs_idx][key] for bs_idx in range(batch_size)]
     for key in state_keys:
         key_to_list[key] = [batch_list[bs_idx][key] for bs_idx in range(batch_size)]
     
@@ -119,7 +118,7 @@ def process_batch(batch_list):
 
 def collate_fn(batch_list):
     
-    return process_batch([_ for _ in batch_list])
+    return process_batch([batch_data for batch_data in batch_list])
 
 
 def get_dataset(config):
@@ -163,29 +162,8 @@ def get_dataloader(config):
     return train_dataloader, val_dataloader, test_dataloader
 
 
-def get_trajs(input_dict, config):
-    num_vehicles = input_dict['cur/meta/num_vehicles']
-    vector_features_list = ['cur/meta/length', 'cur/meta/width', 'cur/meta/class', 'cur/meta/direction']
-    node_features_list = ['cur/state/his/timestamp', 'cur/state/his/x_position', 'cur/state/his/y_position', 'cur/state/his/x_velocity', 'cur/state/his/y_velocity', 'cur/state/his/yaw_angle',]
-    obs_idx = (input_dict['cur/state/his/timestamp'] > 0)[...,-1]
-    vector_feature = torch.cat([torch.unsqueeze(input_dict[feature], dim=1) for feature in vector_features_list], dim=1)
-    node_feature = torch.cat([torch.unsqueeze(input_dict[feature], dim=3) for feature in node_features_list], dim=3)
-    B, N, T, _ = node_feature.shape
-    vector_feature_processed = torch.zeros([B, N, T, 4]).to(node_feature.device)
-    prv_vehicle_idx = 0
-    for batch_idx in range(B):
-        vector_feature_processed[batch_idx, :int(num_vehicles[batch_idx]) - 1, :, :] = torch.repeat_interleave(torch.unsqueeze(vector_feature[int(prv_vehicle_idx):int(prv_vehicle_idx + num_vehicles[batch_idx] - 1), :], dim = 1), repeats=T, dim=1)
-        prv_vehicle_idx += num_vehicles[batch_idx]
-    trajs = torch.cat([node_feature, vector_feature_processed], dim=3)
-    obs_trajs = torch.zeros_like(trajs)
-    occ_trajs = torch.zeros_like(trajs)
-    obs_trajs[obs_idx] = trajs[obs_idx]
-    occ_trajs[~obs_idx] = trajs[~obs_idx]
-    return obs_trajs, occ_trajs
 
-
-def get_road_map(config, batch_first=True):
-    batch_size = config.dataloader_config.batch_size
+def get_road_map(config, batch_size, batch_first=True):
     grid_size_x = config.occupancy_flow_map.grid_size.x
     grid_size_y = config.occupancy_flow_map.grid_size.y
     map_size = (grid_size_x, grid_size_y)

utils/training_utils.py
@@ -1,38 +1,37 @@
 import torch
+
 def parse_data(data, gpu_id, config):
     """
     Parse data from dataloader
     """
     input_dict = {}
     ground_truth_dict = {}
-    num_waypoints = config.task_config.num_waypoints
-    state = ['timestamp', 'x_position', 'y_position', 'x_velocity', 'y_velocity', 'yaw_angle', 'occluded_occupancy_map', 'observed_occupancy_map', 'flow_map']
-    meta_array = ['num_vehicles', 'length', 'width', 'class', 'direction']
-    meta_keys = []
-    state_to_predict = ['occluded_occupancy_map', 'observed_occupancy_map', 'flow_map']
-    state_keys = []
-    state_to_predict_keys = []
-    # for scene_key in ['prv', 'cur', 'nxt']:
-    for scene_key in ['cur']:
-        for key in meta_array:
-            meta_keys.append(f'{scene_key}/meta/{key}')
-        for key in state:
-            state_keys.append(f'{scene_key}/state/his/{key}')
-            if key in state_to_predict:
-                state_to_predict_keys.append(f'{scene_key}/state/pred/{key}')
+    state_keys = [
+            'his/occluded_occupancy_map', 
+            'his/observed_occupancy_map', 
+            'his/flow_map', 
+            'his/observed_trajectories', 
+            'his/occluded_trajectories', 
+            'flow_origin_occupancy_map'
+            ]
+    state_to_predict_keys = [
+        'pred/occluded_occupancy_map', 
+        'pred/observed_occupancy_map', 
+        'pred/flow_map', 
+    ]
+    # # for scene_key in ['prv', 'cur', 'nxt']:
+    # for scene_key in ['cur']:
+    #     for key in meta_array:
+    #         meta_keys.append(f'{scene_key}/meta/{key}')
+    #     for key in state:
+    #         state_keys.append(f'{scene_key}/state/his/{key}')
+    #         if key in state_to_predict:
+    #             state_to_predict_keys.append(f'{scene_key}/state/pred/{key}')
     for key in state_keys:
         input_dict[key] = data[key].to(gpu_id, dtype=torch.float32)
-    for key in meta_keys:
-        input_dict[key] = data[key].to(gpu_id, dtype=torch.float32)
+
     for key in state_to_predict_keys:
-        if data[key].dim() == 4:
-            # OCC
-            batch_size, timestamps, height, width = data[key].shape
-            ground_truth_dict[key] = data[key].to(gpu_id, dtype=torch.float32)
-        elif data[key].dim() == 5:
-            # FLOW
-            batch_size, timestamps, height, width, num_channels= data[key].shape
-            ground_truth_dict[key] = data[key].to(gpu_id, dtype=torch.float32)
+        ground_truth_dict[key] = data[key].to(gpu_id, dtype=torch.float32)
             
     for key, val in input_dict.items():
         if torch.isnan(val).any():
@@ -40,6 +39,7 @@ def parse_data(data, gpu_id, config):
     for key, val in ground_truth_dict.items():
         if torch.isnan(val).any():
             ground_truth_dict[key] = torch.where(torch.isnan(val), torch.zeros_like(val), val)
+    
     return input_dict, ground_truth_dict
 
 def save_checkpoint(model, optimizer, scheduler, epoch, path, global_step):

visualize/animation.py
@@ -275,7 +275,7 @@ if __name__ == '__main__':
     config = get_config('config_12.yaml')
     gridmap = GridMap(config)
     dataset = I24Dataset(config)
-    k = 2
+    k = 3
     name = "scene_1014"
     test_data = np.load(config.paths.processed_data + '/' + name + '.npy', allow_pickle=True).item()
     # visualize(config, test_data, name, vis_occ=True, vis_flow=True, vis_optical_flow=False, valid_dict={'cur': 1})
@@ -285,8 +285,8 @@ if __name__ == '__main__':
     h_idx, w_idx = torch.meshgrid(h, w, indexing="xy")
     # These indices map each (x, y) location to (x, y).
     # [height, width, 2] but storing x, y coordinates.
-    flow_origin_occupancy = torch.from_numpy(test_data['cur/state/pred/observed_occupancy_map'])[k-1] + torch.from_numpy(test_data['cur/state/pred/occluded_occupancy_map'])[k-1]
-    pred_occupancy_map = torch.from_numpy(test_data['cur/state/pred/observed_occupancy_map'])[k]
+    flow_origin_occupancy = torch.from_numpy(test_data['observed_occupancy_map'])[...,-config.task_config.num_waypoints:][...,k-1] + torch.from_numpy(test_data['occluded_occupancy_map'])[...,-config.task_config.num_waypoints:][...,k-1]
+    pred_occupancy_map = torch.from_numpy(test_data['observed_occupancy_map'])[...,-config.task_config.num_waypoints:][...,k]
     print(pred_occupancy_map.shape)
     fig, axes = plt.subplots(1, 3, figsize=(10, 10))
     colors_obs = [
@@ -295,18 +295,23 @@ if __name__ == '__main__':
         (1, 0, 1)  # Red for 1
     ]
     cmap_obs = get_cmap(colors_obs)
-    axes[0].imshow(flow_origin_occupancy.swapaxes(0,1) , cmap=cmap_obs, interpolation='nearest')
+    axes[0].imshow(flow_origin_occupancy, cmap=cmap_obs, interpolation='nearest')
     print(flow_origin_occupancy.shape)
-    pred_flow = torch.from_numpy(test_data['cur/state/pred/flow_map'])
+    pred_flow = torch.from_numpy(test_data['flow_map'])[...,-config.task_config.num_waypoints:,:]
     print(pred_flow.shape)
-    identity_indices = torch.stack((w_idx.T, h_idx.T), dim=-1) 
-    warped_indices = identity_indices + pred_flow[k-1]
-
-    warped_indices[..., 0] = torch.clamp(warped_indices[..., 0], 0, config.occupancy_flow_map.grid_size.x - 1)
-    warped_indices[..., 1] = torch.clamp(warped_indices[..., 1], 0, config.occupancy_flow_map.grid_size.y - 1)
-    w_indices = warped_indices[..., 0].long()
-    h_indices = warped_indices[..., 1].long()
-    flow_wrap_occupancy = flow_origin_occupancy[w_indices, h_indices] 
-    axes[1].imshow(flow_wrap_occupancy.squeeze(), cmap=cmap_obs, interpolation='nearest', )
-
+    identity_indices = torch.stack(
+        (
+            w_idx.T,
+            h_idx.T,
+        ),dim=-1)
+    warped_indices = identity_indices + pred_flow[...,k,:][None, ...]
+    wp_origin = sample(
+        image=flow_origin_occupancy[None, ..., None],
+        warp=warped_indices,
+        pixel_type=0,
+    )
+    axes[1].imshow(np.array(pred_occupancy_map).astype(np.float32), cmap=cmap_obs, interpolation='nearest', )
+    
+    # axes[2].quiver(pred_flow[k][..., 1], pred_flow[k][..., 0], color='black', alpha=0.5)
+    axes[2].imshow(np.array(wp_origin)[0,...,0].astype(np.float32), cmap=cmap_obs, interpolation='nearest', )
     plt.show()